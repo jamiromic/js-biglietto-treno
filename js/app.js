b346358038c624cad3936e23b6b2199d5491c5e0
jamiromic <jamiromic@gmail.com>
2022-07-20 17:18:50 +0200

    add_output_&_arrotond

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 const clientAge = prompt('Gentile Cliente, indichi la sua età');
 const clientKm = prompt('Gentile Cliente, indichi qui i km che intende percorrere');
 
-let priceTotal = (clientKm * 0.21)
+let priceTotal = Math.floor (clientKm * 0.21);
 let discountMin = (priceTotal / 100) * 20;
 let discountMax = (priceTotal / 100) * 40;
 
@@ -15,11 +15,7 @@ if (clientAge > 65) {
     priceTotal = (priceTotal - discountMax)
 }
 
-
-
-
-
-console.log(priceTotal)
+alert('Il prezzo totale da pagare è di € ' +priceTotal)
 
 
 
