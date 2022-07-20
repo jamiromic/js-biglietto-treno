8809cfc47e71f9946b0bdcf4345f59df67f5143d
jamiromic <jamiromic@gmail.com>
2022-07-20 14:28:45 +0200

    add_calc_totalkm

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Applicazione Javascript
+
+const clientAge = prompt('Gentile Cliente, indichi la sua età')
+const clientKm = prompt('Gentile Cliente, indichi qui i km che intende percorrere')
+
+totalKm = (clientKm * 0.21)
+
+console.log(totalKm)
